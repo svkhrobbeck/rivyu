d4c206cad1c556fe8fdb496d51910fde2f587b78
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-04-25 23:31:02 +0500

    add changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Add this line
-      include: "**/*.jsx",
-    }),
-  ],
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
+  plugins: [react()],
 });
