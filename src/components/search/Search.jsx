af50192ab9331b51a29ced65f09e0328c666a2e8
Sukhrobbek Soatov <svkhrobbeck@gmail.com>
2023-05-18 15:52:00 +0500

    update search functional

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -8,20 +8,30 @@ export default function Search() {
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleNavigate = () => {
+    if (!searchQuery) return;
+
     navigate(`/search/${searchQuery.trim().toLowerCase()}`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleNavigate();
   };
 
   return (
     <section className="search">
-      <input
-        className="main-field search__input"
-        type="search"
-        name="search"
-        placeholder="Qidirish"
-        aria-label="Search"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
+      <form className="search__form" onSubmit={handleSubmit}>
+        <input
+          className="main-field search__input"
+          type="search"
+          name="search"
+          placeholder="Qidirish"
+          aria-label="Search"
+          autoComplete="off"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </form>
       <button
         className="search__button button button--green"
         onClick={handleNavigate}
