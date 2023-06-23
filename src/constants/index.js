82407c72f496b90475a2e84238720a870db69e15
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-23 11:06:41 +0500

    Add sidebarLinks to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,18 @@
 export const imageNotShown = "https://via.placeholder.com/320x180?text=Rasm+yuklanmadi";
+
 export const links = [
   { link: "https://tme.to/svkhrobbeck", imgPath: "/images/icon-telegram.svg", alt: "icon telegram" },
   { link: "https://www.facebook.com/svkhrobbeck", imgPath: "/images/icon-facebook.svg", alt: "icon facebook" },
   { link: "https://instagram.com/svkhrobbeck", imgPath: "/images/icon-instagram.svg", alt: "icon instagram" },
+];
+
+export const sidebarLinks = [
+  { name: "Maqolalar", route: "/reviews", image: "/images/icon-pencil.svg" },
+  { name: "Yangiliklar", route: "/news", image: "/images/icon-news.svg" },
+  {
+    name: "Treylerlar",
+    route: "/trailers",
+    image: "/images/icon-videplay.svg",
+  },
+  { name: "Dastur haqida", route: "/about", image: "/images/icon-faq.svg" },
 ];
