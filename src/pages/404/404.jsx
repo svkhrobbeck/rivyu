ae956d4c1e4db6bafc7f1a218af9b6ce3ea5a794
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-23 11:14:36 +0500

    Optimize 404 page component and styles

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,23 +1,23 @@
-import { Link } from "react-router-dom";
+// style
 import "./404.scss";
 
-export default function Page404() {
+import { Link } from "react-router-dom";
+
+const Page404 = () => {
   return (
     <section className="page-404 container">
       <img className="page-404__img" src="/images/404.svg" alt="" />
       <div className="page-404__content">
         <h2 className="page-404__title">Sahifa topilmadi</h2>
         <p className="page-404__desc">
-          "Uzr! Siz yaroqsiz sahifaga tashrif buyurgan ko'rinasiz. Agarda bu
-          bizning xatoyimiz ekanligiga ishonchingiz komil bo'lsa, bizga xabar
-          bering."
+          "Uzr! Siz yaroqsiz sahifaga tashrif buyurgan ko'rinasiz. Agarda bu bizning xatoyimiz ekanligiga ishonchingiz komil bo'lsa, bizga xabar bering."
         </p>
         <Link to={"/"}>
-          <button className="button button--green">
-            Bosh sahifaga qaytish
-          </button>
+          <button className="button button--green">Bosh sahifaga qaytish</button>
         </Link>
       </div>
     </section>
   );
-}
+};
+
+export default Page404;
