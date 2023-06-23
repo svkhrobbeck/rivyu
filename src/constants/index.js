d6fb1dc95e0ea8d076aae3148d166893d5be9672
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-23 11:25:48 +0500

    Add createdAt and lastEdited to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { getZero } from "../utils/utils";
+
 export const imageNotShown = "https://via.placeholder.com/320x180?text=Rasm+yuklanmadi";
 
 export const links = [
@@ -23,3 +25,12 @@ export const aboutIcons = [
   { imgPath: "/firebase.svg", alt: "logo firebase" },
   { imgPath: "/kinoblog.svg", alt: "logo kinoblog" },
 ];
+
+const date = new Date();
+export const createdAt = `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(date.getHours())}:${getZero(
+  date.getMinutes()
+)}`;
+
+export const lastEdited = `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(date.getHours())}:${getZero(
+  date.getMinutes()
+)}`;
