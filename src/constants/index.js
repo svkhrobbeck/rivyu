481ebdac621d6ac7d4c4f692277547ee31c98ff4
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-23 10:56:00 +0500

    Add links to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,6 @@
 export const imageNotShown = "https://via.placeholder.com/320x180?text=Rasm+yuklanmadi";
+export const links = [
+  { link: "https://tme.to/svkhrobbeck", imgPath: "/images/icon-telegram.svg", alt: "icon telegram" },
+  { link: "https://www.facebook.com/svkhrobbeck", imgPath: "/images/icon-facebook.svg", alt: "icon facebook" },
+  { link: "https://instagram.com/svkhrobbeck", imgPath: "/images/icon-instagram.svg", alt: "icon instagram" },
+];
