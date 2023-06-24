d1571bc6f489a4dbe0072869361056afd371a971
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-25 01:10:50 +0500

    Optimize images with image-kit

--- src/components/favorite-post/FavoritePost.jsx
+++ src/components/favorite-post/FavoritePost.jsx
@@ -4,6 +4,7 @@ import "./FavoritePost.scss";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import { useContext } from "react";
+import { firebaseLink, imageKitLink } from "../../constants";
 
 const FavoritePost = () => {
   const { state } = useContext(Context);
@@ -13,7 +14,7 @@ const FavoritePost = () => {
     <>
       {!!image && (
         <section className="favorite-post">
-          <img className="favorite-post__img" src={image} alt={title} width="640" title={title} />
+          <img className="favorite-post__img" src={image?.replace(firebaseLink, imageKitLink)} alt={title} width="640" title={title} />
           <h4 className="favorite-post__title">Eng so'ngi xabar</h4>
           <h5 className="favorite-post__subtitle" title={title}>
             {title}
