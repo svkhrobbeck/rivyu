2b5c092980daa1fe991549146a0df4f7ac672aa9
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-26 23:28:20 +0500

    Add changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,11 +4,30 @@ import "./Home.scss";
 // components
 import { Search, MiniSideBar, FavoritePost, Card } from "../../components";
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../../context/Context";
 
 const Home = () => {
   const { state } = useContext(Context);
+
+  const [postsNum, setPostsNum] = useState(13);
+  const [buttonText, setButtonText] = useState("Ko'proq");
+
+  const updatePostNum = () => {
+    const length = state.arr.length;
+
+    if (length - postsNum < 10) {
+      setPostsNum(length);
+      console.log(length);
+      setButtonText("Kamroq");
+    } else if (length - postsNum >= 10) {
+      setPostsNum(p => (p += 10));
+    }
+    if (postsNum === length) {
+      setPostsNum(13);
+      setButtonText("Ko'proq");
+    }
+  };
 
   document.title = "Kino Blog | Bosh sahifa";
 
@@ -29,7 +48,18 @@ const Home = () => {
           </div>
         </div>
         <div className="home__posts posts-home">
-          <div className="posts-home__inner">{!!state.arr?.length && state.arr?.filter((_, i) => i !== 0).map(item => <Card key={item.id} {...item} />)}</div>
+          <div className="posts-home__inner">
+            {!!state.arr?.length &&
+              state.arr
+                ?.slice(0, postsNum)
+                .filter((_, i) => i !== 0)
+                .map(item => <Card key={item.id} {...item} />)}
+          </div>
+          <div className="home-post__btn-wrapper">
+            <button className="home-post__btn button button--green" onClick={updatePostNum}>
+              {buttonText}
+            </button>
+          </div>
         </div>
       </div>
     </section>
