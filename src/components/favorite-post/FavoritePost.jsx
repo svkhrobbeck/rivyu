5c8a98af4ebe1105b8e68af9f1581ef6ea31bd84
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-26 23:31:31 +0500

    Add changes

--- src/components/favorite-post/FavoritePost.jsx
+++ src/components/favorite-post/FavoritePost.jsx
@@ -15,7 +15,7 @@ const FavoritePost = () => {
       {!!image && (
         <section className="favorite-post">
           <img className="favorite-post__img" src={image?.replace(firebaseLink, imageKitLink)} alt={title} width="640" title={title} />
-          <h4 className="favorite-post__title">Eng so'ngi xabar</h4>
+          <h4 className="favorite-post__title">Eng so'ngi post</h4>
           <h5 className="favorite-post__subtitle" title={title}>
             {title}
           </h5>
