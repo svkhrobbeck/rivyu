e67670fbff5da41d2fd3145831ea43d0ec3e17a0
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-21 16:07:59 +0500

    Remove strictmode

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 import ContextProvider from "./context/Context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </BrowserRouter>
 );
