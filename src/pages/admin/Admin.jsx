3a7fcec807597a4d73d121ae827803d93888cf46
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 01:18:24 +0500

    Add react-helmet to admin page

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,11 +1,16 @@
 // style
 import "./Admin.scss";
 
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const Admin = () => {
   return (
     <section className="admin">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Rivyu | Admin</title>
+      </Helmet>
       <div className="container">
         <div className="admin__btns-wrapper">
           <Link className="button button--green" to="/admin/create">
