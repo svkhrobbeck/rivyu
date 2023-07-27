8da0e36856181f63fa1ad93672c8dc7468cb71bd
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 02:50:51 +0500

    Optimize and fix constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,30 +16,26 @@ export const sidebarLinks = [
   { name: "Bosh Sahifa", route: "/", image: "/images/icon-home.svg" },
   { name: "Maqolalar", route: "/reviews", image: "/images/icon-pencil.svg" },
   { name: "Yangiliklar", route: "/news", image: "/images/icon-news.svg" },
-  {
-    name: "Treylerlar",
-    route: "/trailers",
-    image: "/images/icon-videplay.svg",
-  },
+  { name: "Treylerlar", route: "/trailers", image: "/images/icon-videplay.svg" },
   { name: "Dastur haqida", route: "/about", image: "/images/icon-faq.svg" },
+  { name: "Admin", route: "/admin", image: "/images/icon-admin.svg" },
 ];
 
 export const aboutIcons = [
   { imgPath: "/react.svg", alt: "logo react" },
   { imgPath: "/vite.svg", alt: "logo vite" },
   { imgPath: "/firebase.svg", alt: "logo firebase" },
-  { imgPath: "/kinoblog.svg", alt: "logo kinoblog" },
 ];
 
 export const date = new Date();
 
-export const createdAt = `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(date.getHours())}:${getZero(
-  date.getMinutes()
-)}`;
+export const createdAt = `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(
+  date.getHours()
+)}:${getZero(date.getMinutes())}`;
 
-export const lastEdited = `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(date.getHours())}:${getZero(
-  date.getMinutes()
-)}`;
+export const lastEdited = `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(
+  date.getHours()
+)}:${getZero(date.getMinutes())}`;
 
 export const firebaseLink = "https://firebasestorage.googleapis.com/v0/b/kino-blog.appspot.com";
 
