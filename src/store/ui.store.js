775716b748ec95656232f39d3dd987d586aee476
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 01:13:44 +0500

    Create ui.store

---
+++ src/store/ui.store.js
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "modal":
+      return { modal: payload };
+
+    case "sitenav":
+      return { sitenav: payload };
+
+    case "sitenav-toggle":
+      return { sitenav: !state.sitenav };
+
+    case "sitenav-mini":
+      return { sitenavMini: payload };
+
+    case "sitenav-mini-toggle":
+      return { sitenavMini: !state.sitenavMini };
+
+    default:
+      return state;
+  }
+};
+
+const useUiStore = create(set => ({
+  sitenav: false,
+  modal: false,
+  sitenavMini: false,
+  dispatch: args => set(state => reducer(state, args)),
+}));
+
+export default useUiStore;
