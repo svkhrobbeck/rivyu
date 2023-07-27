fc771644f51e8cfe527a0cca3156f56e9803d828
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 01:17:56 +0500

    Update search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -8,22 +8,17 @@ const Search = () => {
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleNavigate = () => {
-    if (!searchQuery) return;
-
-    navigate(`/search/${searchQuery.trim().toLowerCase()}`);
-  };
-
   const handleSubmit = e => {
     e.preventDefault();
-    handleNavigate();
+    if (!searchQuery) return;
+    navigate(`/search/${searchQuery.trim().toLowerCase()}`);
   };
 
   return (
     <section className="search">
       <form className="search__form" onSubmit={handleSubmit}>
         <input
-          className="main-field search__input"
+          className="main-field main-field--mini search__input"
           type="search"
           name="search"
           placeholder="Qidirish"
@@ -32,10 +27,10 @@ const Search = () => {
           value={searchQuery}
           onChange={e => setSearchQuery(e.target.value)}
         />
+        <button className="search__button button button--green" type="submit">
+          <img src="/images/icon-magnifier.svg" alt="icon magnifier" />
+        </button>
       </form>
-      <button className="search__button button button--green" onClick={handleNavigate}>
-        <img src="/images/icon-magnifier.svg" alt="" />
-      </button>
     </section>
   );
 };
