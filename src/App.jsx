43282c9388cd713a8dc459f561e97cf56da90e4f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 01:09:19 +0500

    Optimize app and add zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // layout
 import Layout from "./layout/Layout";
 
+import usePostsStore from "./store/posts.store";
+import useUiStore from "./store/ui.store";
 
+const App = () => {
+  const { isLoading } = usePostsStore();
+  const { sitenavMini } = useUiStore();
   return (
-    <div className={`app ${state.siteNavMini ? "app--open" : ""}`}>
+    <div className={`app ${sitenavMini ? "app--open" : ""}`}>
+      {/* {isLoading && <Loader />} */}
       <Layout />
     </div>
   );
