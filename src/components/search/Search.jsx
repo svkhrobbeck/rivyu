6b370e63419fc2f10c8fab8dd594deaed07297db
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 16:28:33 +0500

    Change search component button and input, fix styles, add animational opening

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,31 +3,42 @@ import "./Search.scss";
 
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import useUiStore from "../../store/ui.store";
 
 const Search = () => {
+  const { search, dispatch } = useUiStore();
   const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState("");
+  const [query, setQuery] = useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (!searchQuery) return;
-    navigate(`/search/${searchQuery.trim().toLowerCase()}`);
+    if (!query) return;
+    navigate(`/search/${query.trim().toLowerCase()}`);
+    setQuery("");
   };
 
   return (
     <section className="search">
       <form className="search__form" onSubmit={handleSubmit}>
-        <input
-          className="main-field main-field--mini search__input"
-          type="search"
-          name="search"
-          placeholder="Qidirish"
-          aria-label="Search"
-          autoComplete="off"
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-        />
-        <button className="search__button button button--green" type="submit">
+        <div className={`search__input-wrapper ${search ? "" : "search__input-wrapper--close"}`}>
+          <input
+            className="main-field main-field--mini search__input"
+            type="search"
+            name="search"
+            placeholder="Qidirish"
+            aria-label="Search"
+            autoComplete="off"
+            value={query}
+            onFocus={() => dispatch({ type: "search", payload: false })}
+            onChange={e => setQuery(e.target.value)}
+          />
+        </div>
+        <button
+          className={`button button--green search__button ${search ? "search__button--open" : ""}`}
+          type="submit"
+          onClick={() => dispatch({ type: "search", payload: !search })}
+          onBlur={() => dispatch({ type: "search", payload: false })}
+        >
           <img src="/images/icon-magnifier.svg" alt="icon magnifier" />
         </button>
       </form>
