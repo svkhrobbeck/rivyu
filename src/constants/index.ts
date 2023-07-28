c0e149dbc350f0fce1c7054f9d5064b314338d5b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-28 21:59:45 +0500

    Convert constants to typescript

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,15 +1,17 @@
-export const imageNotShown = "https://via.placeholder.com/320x180?text=Rasm+yuklanmadi";
-export const youtubeThumb = "https://www.youtube-nocookie.com/embed/";
-export const firebaseLink = "https://firebasestorage.googleapis.com/v0/b/kino-blog.appspot.com";
-export const imageKitLink = "https://ik.imagekit.io/fj0u394lay/images-kinoblog";
+import { ILinks, ISidebarLinks, ITabs } from "../interfaces/constants.interface";
 
-export const links = [
+export const imageNotShown: string = "https://via.placeholder.com/320x180?text=Rasm+yuklanmadi";
+export const youtubeThumb: string = "https://www.youtube-nocookie.com/embed/";
+export const firebaseLink: string = "https://firebasestorage.googleapis.com/v0/b/kino-blog.appspot.com";
+export const imageKitLink: string = "https://ik.imagekit.io/fj0u394lay/images-kinoblog";
+
+export const links: ILinks[] = [
   { link: "https://tme.to/svkhrobbeck", imgPath: "/images/icon-telegram.svg", alt: "icon telegram" },
   { link: "https://www.facebook.com/svkhrobbeck", imgPath: "/images/icon-facebook.svg", alt: "icon facebook" },
   { link: "https://instagram.com/svkhrobbeck", imgPath: "/images/icon-instagram.svg", alt: "icon instagram" },
 ];
 
-export const sidebarLinks = [
+export const sidebarLinks: ISidebarLinks[] = [
   { name: "Bosh Sahifa", route: "/", image: "/images/icon-home.svg" },
   { name: "Maqolalar", route: "/reviews", image: "/images/icon-pencil.svg" },
   { name: "Yangiliklar", route: "/news", image: "/images/icon-news.svg" },
@@ -18,8 +20,14 @@ export const sidebarLinks = [
   { name: "Admin", route: "/admin", image: "/images/icon-admin.svg" },
 ];
 
-export const aboutIcons = [
+export const aboutIcons: ILinks[] = [
   { imgPath: "/rivyu.svg", alt: "logo rivyu" },
   { imgPath: "/react.svg", alt: "logo react" },
   { imgPath: "/firebase.svg", alt: "logo firebase" },
+];
+
+export const tabs: ITabs[] = [
+  { text: "Maqolalar", type: "reviews" },
+  { text: "Yangiliklar", type: "news" },
+  { text: "Treylerlar", type: "trailers" },
 ];
