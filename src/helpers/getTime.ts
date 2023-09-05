f9fc1e02682937b362e3fb7b200d2c9dd0811ab8
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-05 20:03:28 +0500

    update getTime helper param types

--- src/helpers/getTime.ts
+++ src/helpers/getTime.ts
@@ -1,6 +1,6 @@
 import getZero from "./getZero";
 
-const getTime = (time: number): string => {
+const getTime = (time: number | string): string => {
   const date = new Date(time);
 
   return `${getZero(date.getDate())}.${getZero(date.getMonth() + 1)}.${date.getFullYear()} / ${getZero(
