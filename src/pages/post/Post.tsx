22db1983d459a5abf9b17ee9061a7d7be8211223
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-06 03:08:14 +0500

    Optimize post page

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -5,70 +5,68 @@ import { MiniSideBar, Toast } from "@components/index";
 import YouTube from "react-youtube";
 import { Helmet } from "react-helmet";
 // hooks/utils
-import usePosts from "@hooks/usePosts";
 import { useEffect, useState, FC } from "react";
 import { Link, useParams } from "react-router-dom";
-import { firebaseLink, imageKitLink } from "@helpers/constants";
-import { v4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 // interfaces
 import { IPost } from "@interfaces/posts.interface";
-// store
-import usePostsStore from "@store/posts.store";
 // helpers
 import getTime from "@helpers/getTime";
 import { copyLink } from "@helpers/copyLink";
+import PostsService from "@service/PostsService";
+import { baseApiUrl } from "@helpers/constants";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 // prop type
 type IParams = {
   type: "reviews" | "trailers" | "news";
-  id: string;
+  slug: string;
 };
 
 const Post: FC = (): JSX.Element => {
-  const { post } = usePostsStore();
-  const { getPost } = usePosts();
-  const { type, id } = useParams() as IParams;
+  const { slug } = useParams() as IParams;
 
   const [isShowToast, setIsShowToast] = useState<boolean>(false);
   const handleCloseSuccessToast = (): void => setIsShowToast(false);
   const handleOpenSuccessToast = (): void => setIsShowToast(true);
-  const [data, setData] = useState<IPost>({} as IPost);
-  const isTrailer: boolean = type === "trailers";
-  const image: string = isTrailer ? `https://i.ytimg.com/vi/${data?.videoId}/hq720.jpg` : data?.image;
-  const stateText: string = type === "reviews" ? "maqola" : type === "trailers" ? "treyler" : "yangilik";
+  const [post, setPost] = useState<IPost>({} as IPost);
 
-  getPost(type, id);
-  useEffect(() => setData(post), [post]);
+  const isTrailer: boolean = post.category === "trailers";
+  const image = isTrailer ? `https://i.ytimg.com/vi/${post?.videoId}/hq720.jpg` : baseApiUrl + post?.image;
+  const stateText: string =
+    post.category === "reviews" ? "maqola" : post.category === "trailers" ? "treyler" : "yangilik";
+
+  const getPost = async (slug: string) => {
+    const data = await PostsService.getPost(slug);
+    setPost(data.post);
+  };
+
   useEffect(() => window.scrollTo(0, 0), []);
 
+  useEffect(() => {
+    getPost(slug);
+  }, [slug]);
   return (
     <section className="post">
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{`Rivyu | ${data?.title}`}</title>
+        <title>{`Rivyu | ${post?.title}`}</title>
       </Helmet>
+
       <div className="post__wrapper container">
         <div className="post__article article-post">
           <div className="article-post__inner">
             {!!image && <span className="post-badge">{stateText}</span>}
             {!isTrailer ? (
-              <>
-                {!!image && (
-                  <img
-                    className="article-post__image"
-                    src={image?.replace(firebaseLink, imageKitLink)}
-                    alt={data?.title}
-                  />
-                )}
-              </>
+              <LazyLoadImage className="article-post__image" alt={post.title} effect="blur" src={image} />
             ) : (
-              <YouTube videoId={data.videoId} className="article-post__iframe" />
+              <YouTube videoId={post.videoId} className="article-post__iframe" />
             )}
 
             <div className="article-post__content">
               <div className="article-post__time-like-wrapper">
-                <time className="article-post__time" dateTime={getTime(data?.createdAt)}>
-                  {!!image ? getTime(data.createdAt) : "yuklanmoqda..."}
+                <time className="article-post__time" dateTime={getTime(post?.createdAt)}>
+                  {!!image ? getTime(post.createdAt) : "yuklanmoqda..."}
                 </time>
                 <div className="article-post__buttons-wrapper">
                   <button
@@ -81,20 +79,20 @@ const Post: FC = (): JSX.Element => {
               </div>
               {image && (
                 <>
-                  <h2 className="secondary-title">{data.title}</h2>
-                  {data.description && (
+                  <h2 className="secondary-title">{post.title}</h2>
+                  {post.desc && (
                     <div className="article-post__descs">
-                      {data.description.split("\n").map(desc => (
-                        <p key={v4()} className="article-post__desc">
+                      {post.desc.split("\n").map(desc => (
+                        <p key={uuidv4()} className="article-post__desc">
                           {desc}
                         </p>
                       ))}
                     </div>
                   )}
-                  {!!data?.tags?.length && (
+                  {!!post?.tags?.length && (
                     <div className="article-post__tags">
-                      {data.tags &&
-                        data.tags.map(tag => (
+                      {post.tags &&
+                        post.tags.map(tag => (
                           <Link className="article-post__tag" to={`/search/${tag.toLowerCase()}`} key={tag}>
                             {tag}
                           </Link>
