4907a15b0ed519774f8c58b99ce9b0a48f7a6dfa
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-06 03:07:03 +0500

    Add getPosts async function to main-layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,45 @@
-import { FC } from "react";
-import usePosts from "@hooks/usePosts";
-import usePostsStore from "@store/posts.store";
+import { FC, useEffect } from "react";
 
 // components
 import Router from "@router/Router";
 import { Footer, Header, SideBar } from "@components/index";
+// store
+import usePostsStore from "@store/posts.store";
+// service
+import PostsService from "@service/PostsService";
+// interface
+import { IParams } from "@interfaces/posts.interface";
+import { useSearchParams } from "react-router-dom";
+// constant
+import { limit } from "@helpers/constants";
 
 const Layout: FC = (): JSX.Element => {
-  const { getPosts } = usePosts();
-  const { type } = usePostsStore();
+  const { setPosts, setIsLoading, category, setError, setTotal } = usePostsStore();
+  const [searchParams] = useSearchParams();
 
-  getPosts(type);
+  const params = {
+    category,
+    page: +(searchParams.get("page") || 1),
+    limit,
+  };
+
+  const getPosts = async (params: IParams) => {
+    setIsLoading(true);
+    try {
+      const { total, posts } = await PostsService.getPosts(params);
+      setPosts(posts);
+      setTotal(total);
+    } catch (err) {
+      const error = err as Error;
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPosts(params);
+  }, [category, params.limit, params.page]);
 
   return (
     <>
