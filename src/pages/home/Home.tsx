8277819a6c16be5667eef1967130f3d313440250
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-07 01:51:16 +0500

    Add react toast and call api async function

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,25 +1,55 @@
 // styles
 import "./Home.scss";
+import "react-toastify/dist/ReactToastify.css";
 // components
-import { MiniSideBar, LatestPost, Card, Modal, ModalInner, Tabs, Pagination } from "@components/index";
+import { MiniSidebar, LatestPost, Card, Modal, ModalInner, Tabs, Pagination } from "@components/index";
 import { Helmet } from "react-helmet";
 // store
 import useUiStore from "@store/ui.store";
 // hooks
 import { FC, useEffect } from "react";
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import PostsService from "@service/PostsService";
+import { limit } from "@helpers/constants";
+import { IParams, IPost } from "@interfaces/posts.interface";
+import { useSearchParams } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
 const Home: FC = (): JSX.Element => {
   const { setModal } = useUiStore();
+  const [posts, setPosts] = useState<IPost[]>([] as IPost[]);
+  const [total, setTotal] = useState<number>();
+  const [slug, setSlug] = useState<string>();
   const title: string = "Rostdan ham ushbu maqolani o'chirishni xohlaysizmi?";
-  const [id, setId] = useState<string>("");
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category") || "reviews";
+  const page = +(searchParams.get("page") || 1);
 
-  const func = (): void => {
+  const params = { page, limit, category };
+
+  const getPosts = async (params: IParams) => {
+    try {
+      const { total, posts } = await PostsService.getPosts(params);
+      setPosts(posts);
+      setTotal(total);
+    } catch (err) {
+      const error = err as Error;
+      toast(error.message);
+    } finally {
+    }
+  };
+
+  useEffect(() => {
+    getPosts(params);
+  }, [category, limit, page]);
+
+  const deletePost = async () => {
+    await PostsService.deletePost(slug as string);
+    await getPosts(params);
     setModal(false);
   };
 
-  const click = (id: string) => setId(id);
+  const click = (slug: string) => setSlug(slug);
 
   useEffect(() => window.scrollTo(0, 0), []);
   return (
@@ -28,8 +58,11 @@ const Home: FC = (): JSX.Element => {
         <meta charSet="utf-8" />
         <title>Rivyu | Kino Yangiliklari</title>
       </Helmet>
+
+      <ToastContainer />
+
       <Modal>
-        <ModalInner title={title} func={func} />
+        <ModalInner title={title} func={deletePost} />
       </Modal>
 
       <div className="container">
@@ -38,7 +71,7 @@ const Home: FC = (): JSX.Element => {
             <LatestPost />
           </div>
           <div className="home__side-bar">
-            <MiniSideBar title="So'nggi yangiliklar" />
+            <MiniSidebar title="So'nggi yangiliklar" />
           </div>
         </div>
         <Tabs />
@@ -49,7 +82,7 @@ const Home: FC = (): JSX.Element => {
             <>Maqolalar topilmadi</>
           )}
         </div>
-        <Pagination />
+        <Pagination total={total as number} />
       </div>
     </section>
   );
