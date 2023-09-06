e8339f6b6f0b19e556eb1b475cc5cdefcff68b9b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-06 22:59:06 +0500

    Update pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -15,34 +15,38 @@ const Paginations: FC = (): JSX.Element => {
   const [searchParams, setSearchParams] = useSearchParams();
   const { total } = usePostsStore();
   const pageCount = Math.ceil(total / limit);
+  const currentPage = +(searchParams.get("page") || 1) - 1;
 
   const handlePageClick = (e: { selected: number }) => {
     setSearchParams(useParams(searchParams, "page", e.selected + 1));
   };
 
+  const paginationClassNames = {
+    className: "pagination",
+    activeClassName: "pagination__item--active",
+    activeLinkClassName: "pagination__link--active",
+    disabledClassName: "pagination__disabled",
+    disabledLinkClassName: "pagination__disabled-link",
+    previousClassName: "pagination__previous",
+    previousLinkClassName: "pagination__previous-link",
+    nextClassName: "pagination__next",
+    nextLinkClassName: "pagination__next-link",
+    pageClassName: "pagination__item",
+    pageLinkClassName: "pagination__link",
+    breakClassName: "pagination__item pagination__item--break",
+    breakLinkClassName: "pagination__link",
+  };
+
   return (
     <ReactPaginate
+      {...paginationClassNames}
       breakLabel="..."
       onPageChange={handlePageClick}
       pageRangeDisplayed={3}
       pageCount={pageCount}
       previousLabel=""
       nextLabel=""
-      renderOnZeroPageCount={null}
-      className="pagination"
-      activeClassName="pagination__item--active"
-      activeLinkClassName="pagination__link--active"
-      disabledClassName="pagination__disabled"
-      disabledLinkClassName="pagination__disabled-link"
-      previousClassName="pagination__previous"
-      previousLinkClassName="pagination__previous-link"
-      nextClassName="pagination__next"
-      nextLinkClassName="pagination__next-link"
-      pageClassName="pagination__item"
-      pageLinkClassName="pagination__link"
-      breakClassName="pagination__item pagination__item--break"
-      breakLinkClassName="pagination__link"
-      initialPage={+(searchParams.get("page") || 0) - 1}
+      initialPage={currentPage}
     />
   );
 };
