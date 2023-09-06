09a3231638e46a524011bbad3306bd6ea8bcd1dc
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-07 01:46:10 +0500

    Update sidebar jsx and styles, optimize

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -14,30 +14,32 @@ const SideBar: FC = (): JSX.Element => {
   const hanleSidebarClose = (): void => setSitenav(false);
 
   return (
-    <section className="side-bar">
-      <nav className={`side-bar__nav ${!sitenavMini ? "side-bar__nav--mini" : ""} ${sitenav ? "side-bar__nav--show" : ""}`}>
-        <button className="side-bar__toggler" onClick={() => setSitenavMini(!sitenavMini)}>
+    <section className="sidebar">
+      <nav
+        className={`sidebar__nav ${!sitenavMini ? "sidebar__nav--mini" : ""} ${sitenav ? "sidebar__nav--show" : ""}`}
+      >
+        <button className="sidebar__toggler" onClick={() => setSitenavMini(!sitenavMini)}>
           <img
-            className={`side-bar__toggler-img ${sitenavMini ? "side-bar__toggler-img--open" : ""}`}
+            className={`sidebar__toggler-img ${sitenavMini ? "sidebar__toggler-img--open" : ""}`}
             src="/images/icon-arrow-right.svg"
             alt="icon arrow right"
             width={24}
             height={24}
           />
         </button>
-        <ul className="side-bar__list">
+        <ul className="sidebar__list">
           {sidebarLinks &&
             sidebarLinks.map(link => (
-              <li key={link.name} className={"side-bar__item"} onClick={hanleSidebarClose}>
-                <NavLink className={`side-bar__link ${!sitenavMini && "side-bar__link--mini"}`} to={link.route}>
-                  <img className="side-bar__link-icon" src={link.image} alt="icon" aria-hidden="true" />
-                  <span className="side-bar__link-text">{link.name}</span>
+              <li key={link.name} className={"sidebar__item"} onClick={hanleSidebarClose}>
+                <NavLink className={`sidebar__link ${!sitenavMini && "sidebar__link--mini"}`} to={link.route}>
+                  <img className="sidebar__link-icon" src={link.image} alt="icon" aria-hidden="true" />
+                  <span className="sidebar__link-text">{link.name}</span>
                 </NavLink>
               </li>
             ))}
         </ul>
       </nav>
-      <div className={`side-bar__overlay ${sitenav ? "side-bar__overlay--show" : ""}`} onClick={hanleSidebarClose} />
+      <div className={`sidebar__overlay ${sitenav ? "sidebar__overlay--show" : ""}`} onClick={hanleSidebarClose} />
     </section>
   );
 };
