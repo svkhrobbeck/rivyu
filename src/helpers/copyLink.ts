089a3d515d3b707d31b807335a9a2890ffe20bf3
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-07 01:47:21 +0500

    Delete copyLink helper

--- src/helpers/copyLink.ts
+++
@@ -1,6 +0,0 @@
-export const copyLink = (open: () => void, close: () => void) => {
-  navigator.clipboard.writeText(window.location.href).then(() => {
-    open();
-    setTimeout(close, 2000);
-  });
-};
