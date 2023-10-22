c7bf94003c3e3569819030eff1c4d181157a7858
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-23 01:31:05 +0500

    feat: update errorHandler.middleware

--- server/src/middlewares/errorHandler.middleware.ts
+++ server/src/middlewares/errorHandler.middleware.ts
@@ -4,9 +4,10 @@ import { StatusCodes } from "http-status-codes";
 type ErrorType = Error & { statusCode: number };
 
 const errorHandler = (err: ErrorType, req: Request, res: Response, next: NextFunction) => {
-  const msg = err.message || "something went wrong! try again later";
+  const messages = err.message.split(",") || ["something went wrong! try again later"];
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-  res.status(statusCode).json({ msg });
+
+  res.status(statusCode).json({ messages });
   next();
 };
 
