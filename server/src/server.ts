08cd57b1a7a6c8143214111f3954b048a68092f4
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-23 00:06:06 +0500

    feat:

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import "express-async-errors";
+import express, { Express } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import "dotenv/config";
+// initial
+const app: Express = express();
+const { PORT } = process.env;
+
+// called middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use("*", (req, res) => res.status(401).json({ msg: "invalid endpoint" }));
+app.listen(PORT || 3016, () => console.log(`Server is running on port ${PORT}`));
