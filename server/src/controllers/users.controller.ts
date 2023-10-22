1c551b6d9293f705b29250445667274e69c50dbf
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-23 01:27:22 +0500

    feat: create login controller

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -7,3 +7,10 @@ export const register = async (req: Request, res: Response) => {
   await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
+
+export const login = async (req: Request, res: Response) => {
+  const user = await User.findOne({ email: req.body.email });
+  const access_token = tokenUtil.generate({ userId: user?._id, role: user?.role });
+
+  res.status(StatusCodes.OK).json({ access_token });
+};
