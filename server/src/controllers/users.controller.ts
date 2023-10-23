3454780392b769afe5c8ba0863aac147cb9fcc22
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-24 00:37:11 +0500

    check isValidUser inside login

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { User } from "../models";
 import { StatusCodes } from "http-status-codes";
 import { passwordUtil, tokenUtil } from "../utils";
+import { UnauthenticatedError } from "../errors/custom.errors";
 
 export const register = async (req: Request, res: Response) => {
   const isFirstUser = (await User.countDocuments()) === 0;
@@ -17,6 +18,10 @@ export const register = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
   const user = await User.findOne({ email: req.body.email });
   const payload = { userId: user?._id, role: user?.role };
+
+  const isValidUser =
+    user && (await passwordUtil.compare(req.body.password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
 
   const access_token = tokenUtil.generate(payload);
   res.status(StatusCodes.OK).json({ access_token });
