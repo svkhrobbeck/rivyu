804ebda20ebf4cb870a3e75baa571ffa73cb47f3
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-24 00:15:02 +0500

    feat: make advanced users controller

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import { User } from "../models";
 import { StatusCodes } from "http-status-codes";
-import { tokenUtil } from "../utils";
+import { passwordUtil, tokenUtil } from "../utils";
 
 export const register = async (req: Request, res: Response) => {
+  const isFirstUser = (await User.countDocuments()) === 0;
+  req.body.role = isFirstUser ? "admin" : "user";
+
+  const hashedPassword = await passwordUtil.hash(req.body.password);
+  req.body.password = hashedPassword;
+
   await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req: Request, res: Response) => {
   const user = await User.findOne({ email: req.body.email });
-  const access_token = tokenUtil.generate({ userId: user?._id, role: user?.role });
+  const payload = { userId: user?._id, role: user?.role };
 
+  const access_token = tokenUtil.generate(payload);
   res.status(StatusCodes.OK).json({ access_token });
 };
