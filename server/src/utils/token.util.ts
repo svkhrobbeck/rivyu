8bbae836fe53098dec656b719ae1a093172da56d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-10-24 00:12:34 +0500

    feat: :bug: update and optimize tokenUtil

--- server/src/utils/token.util.ts
+++ server/src/utils/token.util.ts
@@ -1,17 +1,21 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 const { JWT_SECRET, JWT_EXPIRES_IN } = process.env;
 
-const tokenUtil = () => {
-  const generate = (payload: JwtPayload) => {
-    const token = jwt.sign(payload, JWT_SECRET as string, { expiresIn: JWT_EXPIRES_IN as string });
-    return token;
-  };
+interface CustomJwtPayload extends JwtPayload {
+  userId: string;
+  role: string;
+}
 
-  const verify = (token: string) => {
-    const decoded = jwt.verify(token, JWT_SECRET as string);
-    return decoded;
-  };
-
-  return { generate, verify };
+const generate = (payload: JwtPayload) => {
+  const token = jwt.sign(payload, JWT_SECRET as string, {
+    expiresIn: JWT_EXPIRES_IN as string,
+  });
+  return token;
 };
-export default tokenUtil();
+
+const verify = (token: string) => {
+  const decoded = jwt.verify(token, JWT_SECRET as string) as CustomJwtPayload;
+  return decoded;
+};
+
+export default { generate, verify };
